a29505abb31c501e3bf3c43fb9610273ae34a74b
roldanestobio <roldanestobio@gmail.com>
2026-01-28 13:22:18 +0800

    update user access

---
+++ readMe.txt
@@ -0,0 +1,22 @@
+Repo in GitHub hosted on render
+
+Repository in GitHub ->movieCatalogAPI-Estobio
+
+https://moviecatalogapi-estobio.onrender.com -> for backend request(postman)
+
+users: 
+{
+    "email": "dan@mail.com",
+    "password": "dandan123"
+}
+
+{
+    "email": "sampleOnly@mail.com",
+    "password": "sample12345"
+}
+
+admin: 
+{
+    "email": "admin@mail.com",
+    "password": "admin123"
+}
--- routes/movies.js
+++ routes/movies.js
@@ -5,13 +5,13 @@ const {verify, verifyAdmin} = require("../auth");
 
 const router = express.Router();
 
-router.post('/addMovie', verify, movieController.addMovie);
+router.post('/addMovie', verify, verifyAdmin, movieController.addMovie);
 
 router.get('/getMovies', verify, verifyAdmin, movieController.getAllMovie);
 
 router.get('/getMovie/:id', verify, movieController.getMovie);
 
-router.patch('/updateMovie/:id', verify, movieController.updateMovie)
+router.patch('/updateMovie/:id', verify, verifyAdmin, movieController.updateMovie)
 
 router.delete('/deleteMovie/:id', verify, verifyAdmin, movieController.deleteMovie)
 
